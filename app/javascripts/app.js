a6c8adc393213e61fdc3f80ecda7d20ca39a4f3e
Kamil Oleksiuk <koleksiuk20@gmail.com>
2016-12-27 16:18:28 +0100

    connect the tweet form to Web server

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,8 +1,28 @@
 'use strict'
 
 // import Express library
+var mysql = require('mysql');
 var express = require('express');
 var app = express();
+var connection = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'vagrant',
+	password: '',
+	database: 'twitter'
+});
+
+connection.connect(function(err) {
+	if(err) {
+		console.log(err);
+		return;
+	}
+
+  console.log('Connected to the database');
+
+  app.listen(8080, function() {
+  	console.log('Web server is listening on port 8080');
+  });
+});
 
 app.set('views', '../views');
 app.set('view engine', 'ejs');
@@ -13,6 +33,6 @@ app.get('/', function(reg, res) { // GET request
 	res.render('tweets');
 });
 
-app.listen(8080, function() { // listening on port 8080
-	console.log('Web server listening on port 8080');
+app.post('/tweets/create', function(reg, res) {
+	res.send('Creating tweet.');
 });
